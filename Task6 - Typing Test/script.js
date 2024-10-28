5e0705556753fcc5462194dd274fb5eed59e3940
Shubham <40598861+vijit101@users.noreply.github.com>
2024-10-28 21:48:22 +0530

    Typing Test
    Task to code Typing Test

---
+++ Task6 - Typing Test/script.js
@@ -0,0 +1,7 @@
+//Complete the given scaffold to implement all the functionalities mentioned in the problem Statement.
+const sentences = 
+  `The quick brown fox jumps over the lazy dog.
+  Sphinx of black quartz, judge my vow.
+  Pack my box with five dozen liquor jugs.
+  How vexingly quick daft zebras jump!`
+;
