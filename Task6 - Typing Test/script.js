c5a689c43d1ecc6f0ef47155b0f0dc46eb7f781e
Shubham <40598861+vijit101@users.noreply.github.com>
2024-10-28 23:18:25 +0530

    updated the Script to update UI with timer
    added timer to udpate UI

--- Task6 - Typing Test/script.js
+++ Task6 - Typing Test/script.js
@@ -5,3 +5,54 @@ const sentences =
   Pack my box with five dozen liquor jugs.
   How vexingly quick daft zebras jump!`
 ;
+
+// main 
+const textInputElement = document.getElementById("input");
+const sentenceElement = document.getElementById("sentence");
+const startButton = document.getElementById("start-btn");
+const timerElement = document.getElementById("timer");
+const resultElement = document.getElementById("result");
+const typingAreaElement = document.getElementById("typing-area");
+const retakeTestElement = document.getElementById("retry-btn");
+
+const typedInp = textInputElement.value.trim();// trimming starting and end spaces 
+let GlobalTimerTime = 10;
+let timer ;
+
+// all starts when start button is clicked
+
+//main 
+
+// event listners 
+startButton.addEventListener("click",()=>{
+    textInputElement.disabled = false;
+    sentenceElement.innerHTML = sentences;
+    startButton.disabled = true;
+    StartTimer();
+});
+
+
+function StartTimer(){
+    timer = setInterval(() => {
+    GlobalTimerTime--;
+    timerElement.innerHTML = GlobalTimerTime;
+    if(GlobalTimerTime <=0){
+        resultElement.setAttribute("style","display:block");
+        typingAreaElement.setAttribute("style","display:none");
+        retakeTestElement.addEventListener("click",retakeTest);
+        clearTimeout(timer);
+    }
+    
+}, 1000);}
+
+function retakeTest(){
+    GlobalTimerTime = 10;
+    resultElement.setAttribute("style","display:none");
+    typingAreaElement.setAttribute("style","display:inline-block");
+    textInputElement.disabled = true;
+    sentenceElement.innerHTML = "";
+    startButton.disabled = false;
+    typedInp = textInputElement.value.trim();
+}
+
+
